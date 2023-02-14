5b34a4ec31dbe101a0b709732860f3b47ee0003f
Seynzs <thrys.formoso@wvsu.edu.ph>
2023-02-14 21:43:20 +0800

    test

--- firstci4/app/Libraries/Blog.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App\Libraries;
-
-class Blog{
-
-    public function postItem($params){
-        return view('components/post_item', $params);
-    }
-}
--- firstci4/app/Views/components/post_item.php
+++
@@ -1,8 +0,0 @@
-<div class="card col-12 col-sm-6 col-md-4" style="width: 18rem;">
-  <img src="/assets/images/avatar2.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"><?= $title?></h5>
-    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, error impedit. Ipsa pariatur minima fugit fugiat repudiandae, cum nostrum ea ratione dignissimos perferendis ad! Asperiores debitis illum omnis cum minus.</p>
-    <a href="/blog/post" class="btn btn-primary">Read More</a>
-  </div>
-</div>
